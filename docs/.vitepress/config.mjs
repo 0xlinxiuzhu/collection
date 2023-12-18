b45c20959622e203ebdf7b311cbadc8ed3fa52b2
unknown <linhd@cddk.cn>
2023-12-18 15:04:14 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -18,6 +18,8 @@ export default defineConfig({
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'test', link: '/2023-12-15 PGP encryption' }
+
         ]
       }
     ],
