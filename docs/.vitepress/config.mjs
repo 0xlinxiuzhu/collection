33f10dec54a8c7a5f7c92c559180a35c3e11975c
unknown <linhd@cddk.cn>
2023-12-18 20:38:03 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://0xlinxiuzhu.github.io' }
+      { icon: 'github', link: 'https://github.com/0xlinxiuzhu/collection' }
     ]
   }
 })
